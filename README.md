ca83118abc55b7843db378fddff4c21ed059883a
Sujan Lama <moonlightsujan@gmail.com>
2022-10-22 16:07:18 -0500

    * table layout

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # MDP-Assignments
-This repository contains the assignments for the course MDP (Mobile Device Programming) at the Maharishi International University
+
+This repository contains the assignments for the course MDP (Mobile Device Programming) at the
+Maharishi International University
+
+## Assignment 3 Lesson 4
+
+```
+Problem – 1 – Working with TabLayout [15 Points ]
+Dynamically add data to the Table by clicking Add button. Insert two items statically in the table. Add
+more items dynamically by clicking Add button. Add two edit text boxes to get the Android Version and
+Code Name at the top. If the User clicks the Add button, new row will be inserted in the Table Layout.
+
+```
+
+Screenshots:
+
+<p float="left">
+  <img src="/1.png" width="300" />
+  <img src="/2.png" width="300"/> 
+  <img src="/3.png" width="300"/>
+</p>
--- app/src/main/java/com/miu/mdp/MainActivity.kt
+++ app/src/main/java/com/miu/mdp/MainActivity.kt
@@ -1,12 +1,64 @@
 package com.miu.mdp
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.setPadding
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addBtn.setOnClickListener {
+            if (versionEditText.text.isEmpty()) {
+                versionEditText.error = "Please enter a Android Version"
+                return@setOnClickListener
+            }
+
+            if (codeEditText.text.isEmpty()) {
+                codeEditText.error = "Please enter a Android code name"
+                return@setOnClickListener
+            }
+
+            val tableRow = TableRow(applicationContext)
+            tableRow.setPadding(18)
+            tableRow.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.pink))
+
+            val layoutParams = TableLayout.LayoutParams(
+                TableLayout.LayoutParams.MATCH_PARENT,
+                TableLayout.LayoutParams.WRAP_CONTENT
+            )
+            layoutParams.setMargins(0, 24, 0, 0)
+            tableRow.layoutParams = layoutParams
+
+            val versionTextView = TextView(applicationContext)
+            versionTextView.text = versionEditText.text.toString()
+            versionTextView.typeface = Typeface.SERIF
+            versionTextView.textSize = 16f
+            versionTextView.gravity = Gravity.CENTER_HORIZONTAL
+            tableRow.addView(versionTextView)
+
+            val codeTextView = TextView(applicationContext)
+            codeTextView.text = codeEditText.text.toString()
+            codeTextView.textSize = 16f
+            codeTextView.gravity = Gravity.CENTER_HORIZONTAL
+            codeTextView.typeface = Typeface.SERIF
+            tableRow.addView(codeTextView)
+
+            tableLayout.addView(tableRow, layoutParams)
+
+            versionEditText.text.clear()
+            codeEditText.text.clear()
+            codeEditText.clearFocus()
+        }
     }
 }
