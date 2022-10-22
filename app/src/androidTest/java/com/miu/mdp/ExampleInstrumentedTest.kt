1c4f1ac8cf27e3ad725e7aa5287a02d3aebb8c32
Sujan Lama <moonlightsujan@gmail.com>
2022-10-22 15:02:06 -0500

    * changing master to base implementation

--- app/src/androidTest/java/com/miu/mdp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/miu/mdp/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.miu.dinnerdecider
+package com.miu.mdp
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.miu.dinnerdecider", appContext.packageName)
+        assertEquals("com.miu.mdp", appContext.packageName)
     }
 }
