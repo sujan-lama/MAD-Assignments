54cb86f6d5508172620900efad4744f37398549d
Sujan Lama <moonlightsujan@gmail.com>
2022-10-22 18:46:37 -0500

    * walmart login page

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # MDP-Assignments
 This repository contains the assignments for the course MDP (Mobile Device Programming) at the Maharishi International University
+
+## Assignment 3 - Lesson 4
+
+```
+Problem 2: Create a Login page for the Walmart store as per the screen produced
+below. [ 10 Points]
+Apply the Layout knowledge gained from the Lesson – 4.
+
+```
+
+Screenshots:
+
+<p float="left">
+  <img src="/1.png" width="300" />
+  <img src="/2.png" width="300"/> 
+</p>
--- app/src/main/java/com/miu/mdp/MainActivity.kt
+++ app/src/main/java/com/miu/mdp/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.miu.mdp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        forgotPasswordTextView.setOnClickListener {
+            Toast.makeText(this, "Forgot Password Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        signInButton.setOnClickListener {
+            if (emailEditText.text.toString().isEmpty()) {
+                emailEditText.error = "Email is required"
+                return@setOnClickListener
+            }
+
+            if (passwordEditText.text.toString().isEmpty()) {
+                passwordEditText.error = "Password is required"
+                return@setOnClickListener
+            }
+
+            Toast.makeText(
+                this,
+                "Sign In: email: ${emailEditText.text} password: ${passwordEditText.text}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        createAccountButton.setOnClickListener {
+            Toast.makeText(this, "Create Account Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
