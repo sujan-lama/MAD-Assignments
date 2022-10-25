a92857598cd22476defd1a5eab2c19e3c83e2763
Sujan Lama <moonlightsujan@gmail.com>
2022-10-24 23:38:47 -0500

    * walmart shopping page

--- app/src/main/java/com/miu/mdp/MainActivity.kt
+++ app/src/main/java/com/miu/mdp/MainActivity.kt
@@ -3,9 +3,19 @@ package com.miu.mdp
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.miu.mdp.model.User
+import com.miu.mdp.shopping.ShoppingCategoryActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val usersList = mutableListOf(
+        User("Sujan", "Lama", "sujan@gmail.com", "123456"),
+        User("Jane", "Doe", "janedoe@gmail.com", "123456"),
+        User("John", "Smith", "johnsmith@gmail.com", "123456"),
+        User("Jane", "Smith", "janesmith@gmail.com", "123456"),
+        User("John", "Doe", "johndoe@gmail.com", "123456")
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +36,17 @@ class MainActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
+            val user = usersList.find {
+                it.username == emailEditText.text.toString() && it.password == passwordEditText.text.toString()
+            }
+            if (user != null) {
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                startActivity(ShoppingCategoryActivity.newInstance(this, user.username))
+                return@setOnClickListener
+            }
             Toast.makeText(
                 this,
-                "Sign In: email: ${emailEditText.text} password: ${passwordEditText.text}",
+                "Login Failed. Please check your email and password",
                 Toast.LENGTH_SHORT
             ).show()
         }
---
+++ app/src/main/java/com/miu/mdp/model/User.kt
@@ -0,0 +1,8 @@
+package com.miu.mdp.model
+
+data class User(
+    val firstName: String,
+    val lastName: String,
+    val username: String,
+    val password: String
+)
