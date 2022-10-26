3238bfc399f927afc51ae515208300049653c5d4
Sujan Lama <moonlightsujan@gmail.com>
2022-10-25 22:44:15 -0500

    * walmart register page and forgot password

--- README.md
+++ README.md
@@ -1,18 +1,59 @@
 # MDP-Assignments
 This repository contains the assignments for the course MDP (Mobile Device Programming) at the Maharishi International University
 
-## Assignment 3 - Lesson 4
+## Assignment 4 - Lesson 5
 
 ```
-Problem 2: Create a Login page for the Walmart store as per the screen produced
-below. [ 10 Points]
-Apply the Layout knowledge gained from the Lesson – 4.
+Problem 1: Create a Login page for the Walmart store
+Task :1 - Home page : activity_main.xml – Reuse the Home page from Lesson 4 
 
+Task 2: We don’t have a database. So, create a User class which consists of firstname, lastname,
+username (will be an email id) and password instances. Make a constructor to initialize the
+values.
+
+Inside your MainActivity.kt, create an ArrayList for the type of User, and store at least five users
+to validate login.
+
+If the user click the Sign In button, validate the user in the ArrayList, if it match with your data,
+then your Main Activity need to open the another intent Shopping Category activity as per the
+below screen. From MainActivity.kt pass the username to ShoppingCategory activity and display
+at the top welcome username as per the screen shot. If the user clicks the image, just display the
+Toast message, that “You have chosen the Clothing category of shopping”. Similarly based on
+the selection of image category display the toast message as per the chosen image.
+
+Task 4: Shoppping_activity.xml
+
+Task 5: From the above page if the user click Create a new account, this page directs to another
+intent RegisterActivity as below
 ```
 
 Screenshots:
 
+Sign in flow:
+
 <p float="left">
-  <img src="/1.png" width="300" />
-  <img src="/2.png" width="300"/> 
+  <img src="/1.png" width="250" />
+  <img src="/2.png" width="250"/> 
+  <img src="/3.png" width="250"/> 
+  <img src="/4.png" width="250"/> 
 </p>
+
+Create new account flow:
+
+<p float="left">
+  <img src="/5.png" width="250" />
+  <img src="/6.png" width="250"/> 
+  <img src="/7.png" width="250"/> 
+  <img src="/8.png" width="250"/> 
+</p>
+
+Forgot password flow:
+
+<p float="left">
+  <img src="/9.png" width="250" />
+  <img src="/10.png" width="250"/> 
+  <img src="/11.png" width="250"/> 
+</p>
+
+
+
--- app/src/main/java/com/miu/mdp/MainActivity.kt
+++ app/src/main/java/com/miu/mdp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.miu.mdp
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AppCompatActivity
 import com.miu.mdp.model.User
+import com.miu.mdp.register.RegisterActivity
 import com.miu.mdp.shopping.ShoppingCategoryActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,17 +31,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         forgotPasswordTextView.setOnClickListener {
-            Toast.makeText(this, "Forgot Password Clicked", Toast.LENGTH_SHORT).show()
+            if (emailEditText.text.toString().isEmpty()) {
+                emailEditText.error = "Email is required"
+                Toast.makeText(this, "Email is required", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val user = usersList.find { it.username == emailEditText.text.toString() }
+            if (user == null) {
+                Toast.makeText(this, "User not found", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(user.username))
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Password Recovery")
+            intent.putExtra(Intent.EXTRA_TEXT, "Your password is ${user.password}")
+            intent.type = "message/rfc822"
+            try {
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(this, "No email app found", Toast.LENGTH_LONG).show()
+            }
         }
 
         signInButton.setOnClickListener {
             if (emailEditText.text.toString().isEmpty()) {
                 emailEditText.error = "Email is required"
+                Toast.makeText(this, "Email is required", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
             if (passwordEditText.text.toString().isEmpty()) {
                 passwordEditText.error = "Password is required"
+                Toast.makeText(this, "Password is required", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
@@ -51,8 +81,17 @@ class MainActivity : AppCompatActivity() {
             ).show()
         }
 
+        val resultLauncher = registerForActivityResult(StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data = result.data
+                val user = data?.getParcelableExtra<User>("user") as User
+                usersList.add(user)
+                Log.d("MainActivity", "userList: $usersList")
+                Toast.makeText(this, "Account Created Successfully", Toast.LENGTH_SHORT).show()
+            }
+        }
         createAccountButton.setOnClickListener {
-            Toast.makeText(this, "Create Account Clicked", Toast.LENGTH_SHORT).show()
+            resultLauncher.launch(RegisterActivity.newInstance(this))
         }
     }
 }
