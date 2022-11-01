70eff1cb05264b30695bd7a21c4127f2efff161b
Sujan Lama <moonlightsujan@gmail.com>
2022-10-31 23:11:26 -0500

    * quiz app for input controls

--- README.md
+++ README.md
@@ -1,12 +1,30 @@
 # MDP-Assignments
 This repository contains the assignments for the course MDP (Mobile Device Programming) at the Maharishi International University
 
-Assignments:
+## Assignment 5 - Lesson 6
 
-Dinner Decider : https://github.com/sujan-lama/MDP-Assignments/tree/dinner-decider
+```
+Problem1: Quiz App [ 15 Points]
+Make a Quiz app. Prepare the quiz questions from the Kotlin Fundamentals knowledge.
+Following are the requirements
+• Create a first question from Kotlin fundamentals with three choices of Radio buttons
+using RadioGroup.
+• Create a second question from Kotlin fundamentals with three choices of Check Box.
+• At the end of the Screen create two Buttons Submit and Reset
+• If the user clicks the submit button, open an Alert dialog which shows the current date
+and time submitted with the score. (Each question carries 50 points). Example if only one
+is correct, your dialog message will be “Congratulations! You submitted on current date
+and time, Your achieved 50%”
+[ Date and time format are up to you]
+• If the user selects the Reset button clear selected choices and the screen will be ready to
+take the quiz.
+```
 
-Working with tab layout: https://github.com/sujan-lama/MDP-Assignments/tree/working-with-tab-layout
+Screenshots:
 
-Walmart login page: https://github.com/sujan-lama/MDP-Assignments/tree/walmart-login-page
-
-Walmart shopping, register activity and forgot password flow: https://github.com/sujan-lama/MDP-Assignments/tree/walmart-shopping-register
+<p float="left">
+  <img src="/1.png" width="300" />
+  <img src="/2.png" width="300"/> 
+  <img src="/3.png" width="300"/> 
+  <img src="/4.png" width="300"/> 
+</p>
--- app/src/main/java/com/miu/mdp/MainActivity.kt
+++ app/src/main/java/com/miu/mdp/MainActivity.kt
@@ -1,12 +1,105 @@
 package com.miu.mdp
 
 import android.os.Bundle
+import android.widget.CompoundButton
+import android.widget.CompoundButton.OnCheckedChangeListener
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnCheckedChangeListener {
 
+    private var answer1: String = "Android AVD Manager"
+    private var answer2 = mutableListOf("INVISIBLE", "GONE")
+
+    private var userAnswer1: String = ""
+    private var userAnswer2 = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Question 1
+        radioGroup1.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.radioButton1 -> userAnswer1 = radioButton1.text.toString()
+                R.id.radioButton2 -> userAnswer1 = radioButton2.text.toString()
+                R.id.radioButton3 -> userAnswer1 = radioButton3.text.toString()
+                R.id.radioButton4 -> userAnswer1 = radioButton4.text.toString()
+            }
+        }
+
+        // Question 2
+        checkBox1.setOnCheckedChangeListener(this)
+        checkBox2.setOnCheckedChangeListener(this)
+        checkBox3.setOnCheckedChangeListener(this)
+        checkBox4.setOnCheckedChangeListener(this)
+        checkBox5.setOnCheckedChangeListener(this)
+
+        submitBtn.setOnClickListener {
+
+            if (userAnswer1.isEmpty() || userAnswer2.isEmpty()) {
+                Toast.makeText(this, "Please answer all questions", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            var score = 0
+            if (userAnswer1 == answer1) {
+                score += 50
+            }
+            if (userAnswer2.containsAll(answer2)) {
+                score += 50
+            }
+            // current date and time in format
+            val date = Calendar.getInstance().time
+            val formatter = SimpleDateFormat("MM/dd/yyyy, hh:mm:ss a", Locale.getDefault())
+            val currentDateTime = formatter.format(date)
+
+
+            val message =
+                if (score > 0) "Congratulations!!! You submitted on $currentDateTime. You achieved $score%"
+                else "Sorry, You submitted on $currentDateTime. You achieved $score%"
+            AlertDialog.Builder(this)
+                .setTitle("Result")
+                .setMessage(message)
+                .setPositiveButton("OK") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .show()
+
+
+        }
+
+
+        resetBtn.setOnClickListener {
+            radioGroup1.clearCheck()
+            resetCheckbox()
+        }
+
+    }
+
+    private fun resetCheckbox() {
+        checkBox1.isChecked = false
+        checkBox2.isChecked = false
+        checkBox3.isChecked = false
+        checkBox4.isChecked = false
+        checkBox5.isChecked = false
+    }
+
+    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+        when (buttonView?.id) {
+            R.id.checkBox1 -> addAnswer("INVISIBLE", isChecked)
+            R.id.checkBox2 -> addAnswer("GONE", isChecked)
+            R.id.checkBox3 -> addAnswer("VISIBLE", isChecked)
+            R.id.checkBox4 -> addAnswer("INVISIBLE", isChecked)
+            R.id.checkBox5 -> addAnswer("GONE", isChecked)
+        }
+    }
+
+    private fun addAnswer(answer: String, isChecked: Boolean) {
+        if (isChecked) userAnswer2.add(answer) else userAnswer2.remove(answer)
     }
 }
