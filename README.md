2f4808239acb6607503538bb63f18a329f934ef7
Sujan Lama <moonlightsujan@gmail.com>
2022-11-01 00:42:35 -0500

    * electronics page

--- README.md
+++ README.md
@@ -1,30 +1,23 @@
 # MDP-Assignments
-This repository contains the assignments for the course MDP (Mobile Device Programming) at the Maharishi International University
+
+This repository contains the assignments for the course MDP (Mobile Device Programming) at the
+Maharishi International University
 
 ## Assignment 4 - Lesson 5
 
 ```
-Problem 1: Create a Login page for the Walmart store
-Task :1 - Home page : activity_main.xml – Reuse the Home page from Lesson 4 
+Problem:2 This screen is from the Lesson-4 Walmart homework. It shows 4 shopping
+categories.
 
-Task 2: We don’t have a database. So, create a User class which consists of firstname, lastname,
-username (will be an email id) and password instances. Make a constructor to initialize the
-values.
+Create RecyclerListView for the Electronics Items as mentioned below. [ 20 Points]
+If the user clicks the item from the list, you have to show the detailed view as shown in the
+screen below.
 
-Inside your MainActivity.kt, create an ArrayList for the type of User, and store at least five users
-to validate login.
+default login credentials:
+email: "sujan@gmail.com", 
+password: "123456"
 
-If the user click the Sign In button, validate the user in the ArrayList, if it match with your data,
-then your Main Activity need to open the another intent Shopping Category activity as per the
-below screen. From MainActivity.kt pass the username to ShoppingCategory activity and display
-at the top welcome username as per the screen shot. If the user clicks the image, just display the
-Toast message, that “You have chosen the Clothing category of shopping”. Similarly based on
-the selection of image category display the toast message as per the chosen image.
-
-Task 4: Shoppping_activity.xml
-
-Task 5: From the above page if the user click Create a new account, this page directs to another
-intent RegisterActivity as below
+other login credentials are in MainActivity.kt list of users
 ```
 
 Screenshots:
@@ -35,24 +28,8 @@ Sign in flow:
   <img src="/1.png" width="250" />
   <img src="/2.png" width="250"/> 
   <img src="/3.png" width="250"/> 
-  <img src="/4.png" width="250"/> 
-</p>
-
-Create new account flow:
-
-<p float="left">
-  <img src="/5.png" width="250" />
-  <img src="/6.png" width="250"/> 
-  <img src="/7.png" width="250"/> 
-  <img src="/8.png" width="250"/> 
-</p>
-
-Forgot password flow:
-
-<p float="left">
-  <img src="/9.png" width="250" />
-  <img src="/10.png" width="250"/> 
-  <img src="/11.png" width="250"/> 
+  <img src="/12.png" width="250"/> 
+  <img src="/13.png" width="250"/> 
 </p>
 
 
--- app/src/main/java/com/miu/mdp/shopping/ShoppingCategoryActivity.kt
+++ app/src/main/java/com/miu/mdp/shopping/ShoppingCategoryActivity.kt
@@ -2,15 +2,16 @@ package com.miu.mdp.shopping
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.miu.mdp.R
+import com.miu.mdp.model.Product
+import com.miu.mdp.products.ProductActivity
 import kotlinx.android.synthetic.main.activity_shopping_category.*
 
 class ShoppingCategoryActivity : AppCompatActivity(), View.OnClickListener {
-
 
     companion object {
         const val USERNAME = "userName"
@@ -21,6 +22,50 @@ class ShoppingCategoryActivity : AppCompatActivity(), View.OnClickListener {
             return intent
         }
     }
+
+    private val electronicProductList = arrayListOf(
+        Product(
+            itemId = "1",
+            title = "Samsung Galaxy S21",
+            price = 1000.0,
+            color = "Black",
+            desc = "Samsung Galaxy S21 is the latest smartphone from Samsung. It has a 6.2-inch display, 8GB RAM, 128GB storage, and a 4000mAh battery. It has a 12MP + 64MP + 12MP triple rear camera and a 10MP front camera. It runs on Android 11 and is powered by a 4000mAh battery.",
+            image = R.drawable.s21
+        ),
+        Product(
+            itemId = "2",
+            title = "Apple iPhone 12",
+            price = 800.0,
+            color = "Black",
+            desc = "Apple iPhone 12 is the latest smartphone from Apple. It has a 6.1-inch display, 4GB RAM, 64GB storage, and a 2815mAh battery. It has a 12MP + 12MP + 12MP triple rear camera and a 12MP front camera. It runs on iOS 14 and is powered by a 2815mAh battery.",
+            image = R.drawable.iphone12
+        ),
+        Product(
+            itemId = "3",
+            title = "Google Pixel 5",
+            price = 700.0,
+            color = "Black",
+            desc = "Google Pixel 5 is the latest smartphone from Google. It has a 6.0-inch display, 8GB RAM, 128GB storage, and a 4080mAh battery. It has a 12.2MP + 16MP + 12.2MP triple rear camera and a 8MP front camera. It runs on Android 11 and is powered by a 4080mAh battery.",
+            image = R.drawable.pixel5
+        ),
+        Product(
+            itemId = "4",
+            title = "OnePlus 8T",
+            price = 600.0,
+            color = "Black",
+            desc = "OnePlus 8T is the latest smartphone from OnePlus. It has a 6.55-inch display, 12GB RAM, 256GB storage, and a 4500mAh battery. It has a 48MP + 16MP + 5MP triple rear camera and a 16MP front camera. It runs on Android 11 and is powered by a 4500mAh battery.",
+            image = R.drawable.oneplus8t
+        ),
+        Product(
+            itemId = "5",
+            title = "Samsung Galaxy S20 FE",
+            price = 500.0,
+            color = "Black",
+            desc = "Samsung Galaxy S20 FE is the latest smartphone from Samsung. It has a 6.5-inch display, 8GB RAM, 128GB storage, and a 4500mAh battery. It has a 12MP + 12MP + 8MP triple rear camera and a 32MP front camera. It runs on Android 11 and is powered by a 4500mAh battery.",
+            image = R.drawable.s20fe
+        ),
+    )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,11 +90,12 @@ class ShoppingCategoryActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v) {
             category_electronics, category_electronics_text -> {
-                Toast.makeText(
-                    this,
-                    "You have chosen the Electronics category of shopping”",
-                    Toast.LENGTH_SHORT
-                ).show()
+                startActivity(
+                    ProductActivity.newInstance(
+                        this,
+                        productList = electronicProductList
+                    )
+                )
             }
             category_clothing, category_clothing_text -> {
                 Toast.makeText(
